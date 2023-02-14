f03f7e7f68fd818e3ad6fac9a1f43c361cb8c7c5
Danieldandandan <zchu2@ucsc.edu>
2023-02-13 17:47:00 -0800

    fix injection

--- demo/src/main/java/com/example/demo/User/UserController.java
+++ demo/src/main/java/com/example/demo/User/UserController.java
@@ -1,19 +1,20 @@
 package com.example.demo.User;
 
 import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 // API lyer
+
 @RestController
 @RequestMapping(path = "api/v1/user")
 public class UserController {
     
     private final UserService userService;
-    public UserController () {
-        userService = new UserService();
-    }
 
+    @Autowired
     public UserController (UserService userService) {
         this.userService = userService;
     }
--- demo/src/main/java/com/example/demo/User/UserService.java
+++ demo/src/main/java/com/example/demo/User/UserService.java
@@ -1,10 +1,15 @@
 package com.example.demo.User;
 
 import java.util.*;
+
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 
+@Service
 public class UserService {
+
     public List<User> getUser(){
 		return List.of(
 			new User(
@@ -16,4 +21,5 @@ public class UserService {
 			)
 		);
 	}
+	
 }
